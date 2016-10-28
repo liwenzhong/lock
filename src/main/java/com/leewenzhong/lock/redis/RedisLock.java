0394fa42f70813e3abaad1148ea7400ce1009732
wenzhong.li <wenzhong.li@qunar.com>
2016-10-28 14:37:35 +0800

    init

--- src/main/java/com/leewenzhong/lock/redis/RedisLock.java
+++ src/main/java/com/leewenzhong/lock/redis/RedisLock.java
@@ -1,5 +1,12 @@
 package com.lee.lock;
 
+import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by liwenzhong on 16-10-26.
  */
@@ -7,44 +14,123 @@ public class RedisLock {
 
 	private IRedisOp redisOp;
 
-	private static long DEFAULT_TIME = 10L; //default 10 seconds
-	private static String DEFAULT_VALUE = "_DEFAULT_VALUE";
+	private static long DEFAULT_TIME = 10000L; //default 10 seconds
 
-	public boolean lock(String key) {
-		return this.lock(key, DEFAULT_VALUE, DEFAULT_TIME);
-	}
+	private static final String UID = genUid();
 
-	public boolean lock(String key, long expireSecond) {
-		return this.lock(key, DEFAULT_VALUE, expireSecond);
+	/**
+	 * 生成uid， 尽量每台机器不同!!!这里时一个参考实现。
+	 *
+	 * @return
+	 */
+	private static String genUid() {
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			return RandomStringUtils.random(8);
+		}
 	}
 
 	/**
-	 * Acquires the lock. none block
+	 * 生成locked value
+	 *
+	 * @return
 	 */
-	public boolean lock(String key, String value, long expireSecond) {
-		long ttl = this.redisOp.ttl(key);
-		//case 1: ttl>0  key exists and has expireTime
-		//case 2: ttl=-1 key has no expireTime
-		//case 3: ttl=-2 key does not exist
-		if (ttl > 0L) {
-			//already locked
-			return false;
-		} else if (ttl == -1L) {
-			this.redisOp.del(key);
-			return false;
+	private static String genLockedValue() {
+		return String.format("%s_%s", UID, Thread.currentThread().getId());
+	}
+
+	/**
+	 * get locked value
+	 *
+	 * @param key
+	 * @return
+	 */
+	public String getLockedValue(String key) {
+		return this.redisOp.get(key);
+	}
+
+	/**
+	 * blocking lock! wait for the lock until available
+	 *
+	 * @param key
+	 */
+	public void lock(String key) {
+		if (StringUtils.isBlank(key)) {
+			throw new IllegalArgumentException("key is blabk!");
 		}
 
-		boolean success = this.redisOp.setNx(key, value);
-		if (success) {
-			success = this.redisOp.expire(key, expireSecond);
+		while (!this.redisOp.setNx(key, genLockedValue())) ;
+	}
+
+	/**
+	 * blocking lock! wait for the lock until available or interrupted or timeout!
+	 *
+	 * @param key
+	 * @param time 等待的timeout时间
+	 * @param unit
+	 * @throws InterruptedException
+	 */
+	public void lock(String key, long time, TimeUnit unit) throws InterruptedException {
+		if (StringUtils.isBlank(key) || time < 1L || unit == null) {
+			throw new IllegalArgumentException("key/time/unit is invalid!");
 		}
-		return success;
+
+		long expireTime = System.currentTimeMillis() + unit.toMillis(time);
+		while (System.currentTimeMillis() - expireTime <= 0) {
+			if (this.redisOp.setNx(key, genLockedValue())) {
+				return;
+			}
+
+			if (Thread.interrupted()) {
+				throw new InterruptedException();
+			}
+		}
+
+		throw new InterruptedException("wait for lock timeout!");
+	}
+
+	/**
+	 * none blocking lock
+	 *
+	 * @param key
+	 * @return
+	 */
+	public boolean tryLock(String key) {
+		return this.tryLock(key, genLockedValue(), DEFAULT_TIME);
+	}
+
+	/**
+	 * none blocking lock
+	 *
+	 * @param key
+	 * @param expireSecond
+	 * @return
+	 */
+	public boolean tryLock(String key, long expireSecond) {
+		return this.tryLock(key, genLockedValue(), expireSecond);
+	}
+
+	/**
+	 * Acquires the lock. none blocking
+	 */
+	private boolean tryLock(String key, String value, long expireMs) {
+		return this.redisOp.setNx(key, value, expireMs);
 	}
 
 	/**
 	 * Releases the lock.
 	 */
 	public void unlock(String key) {
+		if (StringUtils.equals(getLockedValue(key), genLockedValue())) {
+			this.redisOp.del(key);
+		}
+	}
+
+	/**
+	 * force to release the lock.
+	 */
+	public void destoryLock(String key) {
 		this.redisOp.del(key);
 	}
 }
