27c32c20e746e18415cd0568a28c02a30b6c94d1
wenzhong.li <wenzhong.li@qunar.com>
2016-10-28 14:34:39 +0800

    init

--- src/main/java/com/leewenzhong/lock/redis/IRedisOp.java
+++ src/main/java/com/leewenzhong/lock/redis/IRedisOp.java
@@ -4,27 +4,32 @@ package com.lee.lock;
  * Created by liwenzhong on 16-10-26.
  */
 public interface IRedisOp {
+
 	boolean setNx(String key, String value);
 
-	void del(String key);
+	/**
+	 * 不存在时设置key, value, 且设置过期时间
+	 * redis server >=2.6.12 http://redisdoc.com/string/set.html
+	 *
+	 * @param key
+	 * @param value
+	 * @param time  过期时间，单位毫秒,millisecond
+	 * @return
+	 */
+	boolean setNx(String key, String value, long time);
 
 	/**
-	 * set expire time for key
-	 *
-	 * @param key
-	 * @param time second
-	 * @return
-	 */
-	boolean expire(String key, long time);
- 
-        /**
-	 * get expire time for key
+	 * 获取key的剩余生存时间，单位毫秒, millisecond
 	 *
 	 * @param key
 	 * @return
-                ttl>0  key exists and has expireTime
-		ttl=-1 key has no expireTime
-		ttl=-2 key does not exist 
+	       ttl>0  key exists and has expireTime
+	       ttl=-1 key has no expireTime
+	       ttl=-2 key does not exist
 	 */
-	long ttl(String key);
+	long pttl(String key);
+
+	String get(String key);
+
+	void del(String key);
 }
